371702dc429bc030fcb4b8180eda54d8f06403b8
seeya <loohongchai@gmail.com>
2021-06-24 16:31:59 +0800

    Modified email service to use insecure tls config

--- internal/services/email/send.go
+++ internal/services/email/send.go
@@ -2,12 +2,14 @@ package email
 
 import (
 	"fmt"
-	"net/smtp"
+	//"net/smtp"
 	"time"
-
+	"crypto/tls"
 	"gitlab.com/pennersr/shove/internal/services"
+	jwemail "github.com/jordan-wright/email"
 )
 
+/*
 func (ec EmailConfig) send(from string, to []string, body []byte, fc services.FeedbackCollector) error {
 	t := time.Now()
 	err := smtp.SendMail(fmt.Sprintf("%s:%d", ec.EmailHost, ec.EmailPort), nil, from, to, body)
@@ -19,4 +21,20 @@ func (ec EmailConfig) send(from string, to []string, body []byte, fc services.Fe
 		return err
 	}
 	return nil
+}
+*/
+
+func (ec EmailConfig) send(from string, to []string, body []byte, fc services.FeedbackCollector) error {
+        t := time.Now()
+
+        email, _ := jwemail.NewEmailFromReader(bytes.NewReader(body))
+        err := email.SendWithStartTLS(fmt.Sprintf("%s:%d", ec.EmailHost, ec.EmailPort), nil, &tls.Config{InsecureSkipVerify: true})
+        duration := time.Since(t)
+        fc.CountPush(serviceID, err == nil, duration)
+
+        if err != nil {
+                ec.Log.Printf("[ERROR] Send failed: %s", err)
+                return err
+        }
+        return nil
 }
